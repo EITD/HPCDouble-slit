b62a9f544a41f8bed2fcf4dd19571a229611dfb4
EITD <islahou2022@gmail.com>
2024-05-05 15:07:24 +0200

    fit model

--- mpi/fit.py
+++ mpi/fit.py
@@ -5,14 +5,14 @@ from scipy.optimize import curve_fit
 sizes = np.array([1, 2, 4, 8, 16, 32, 64, 128, 256]) 
 times = np.array([19.371616717, 18.403196836, 16.083139587, 15.866991874, 15.927508032, 15.984533610, 16.177270197, 20.782886575, 23.394011481])
 
-def performance_model(p, a, b):
-    return a/p + b/np.sqrt(p)
+def performance_model(p, a, b, c):
+    return a/p + b*p**c
 
 params, params_covariance = curve_fit(performance_model, sizes, times)
 
 plt.figure(figsize=(10, 7))
 plt.plot(sizes, times, marker='o', linestyle='-', label='Measured Data')
-plt.plot(sizes, performance_model(sizes, *params), color='red', label='Fitted function: a/p + b/sqrt(p)')
+plt.plot(sizes, performance_model(sizes, *params), color='red', label='Fitted function: a/p + b*p^c')
 plt.xlabel('Number of Processes')
 plt.ylabel('Execution Time (seconds)')
 plt.title('Performance Modeling of Distributed Matrix Multiplication')
@@ -20,4 +20,4 @@ plt.xticks(sizes)
 plt.legend()
 plt.savefig('model.png')
 
-print("Fitted parameters: a =", params[0], ", b =", params[1])
+print("Fitted parameters: a =", params[0], ", b =", params[1], ", c =", params[2])
