3fd10dc63b38f81cf87a7e17b49c7d2db39d41ec
AMomozZz <malongjordanl@126.com>
2024-05-14 14:53:51 +0200

    add ipc

--- openmp/plot.py
+++ openmp/plot.py
@@ -7,20 +7,7 @@ exec_time = [1.77119931, 1.60838459, 0.88118148, 0.53694686, 0.37435735, 0.32451
 cpu_usage = [99.78, 99.65, 99.17, 98.35, 97.35, 96.53, 95.52, 93.53, 88.65]
 frontend_idle = [0.01, 0.063, 0.082, 0.074, 0.08, 0.133, 0.085, 0.088, 0.451]
 backend_idle = [77.15, 86.66, 87.84, 90.00, 92.67, 94.78, 95.85, 97.62, 5.57]
-ipc = [[1.78, 1.77, 1.78, 1.77, 1.77, 1.78, 1.77, 1.78, 1.77, 1.77],
-       [0.98, 0.98, 0.98, 0.97, 1.02, 1.02, 0.98, 0.98, 0.98, 1.03],
-       [0.92, 0.91, 0.93, 0.92, 0.92, 0.92, 0.92, 0.92, 0.92, 0.92],
-       [0.77, 0.77, 0.77, 0.77, 0.78, 0.77, 0.77, 0.77, 0.78, 0.77],
-       [0.60, 0.60, 0.60, 0.60, 0.60, 0.59, 0.59, 0.59, 0.59, 0.60],
-       [0.41, 0.39, 0.40, 0.39, 0.41, 0.40, 0.41, 0.39, 0.40, 0.40],
-       [0.25, 0.25, 0.27, 0.24, 0.26, 0.26, 0.25, 0.27, 0.26, 0.25],
-       [0.16, 0.16, 0.17, 0.16, 0.16, 0.16, 0.15, 0.15, 0.16, 0.16],
-       [0.18, 0.19, 0.18, 0.18, 0.18, 0.18, 0.18, 0.18, 0.18, 0.18]]
-
-means = np.mean(ipc, axis=1)
-print(means)
-
-exit()
+ipc = [1.774, 0.992, 0.92, 0.772, 0.596, 0.4, 0.256, 0.159, 0.181]
 
 # Plot
 fig, ax1 = plt.subplots(figsize=(10, 6))
@@ -28,8 +15,8 @@ fig, ax1 = plt.subplots(figsize=(10, 6))
 # First axis (left y-axis)
 color = 'tab:red'
 ax1.set_xlabel('Threads')
-ax1.set_ylabel('Execution Time (s)', color=color)
-ax1.plot(threads, exec_time, color=color, label='Execution Time (s)', marker='o')
+ax1.set_ylabel('IPC', color=color)
+ax1.plot(threads, ipc, color=color, label='IPC', marker='o')
 ax1.tick_params(axis='y', labelcolor=color)
 
 # Second axis (right y-axis)
@@ -53,4 +40,28 @@ plt.xticks(threads, labels=threads)
 plt.xscale('log', base=2) 
 plt.grid(True)
 plt.savefig('scale.png')
+
+# Plot
+fig2, ax3 = plt.subplots(figsize=(10, 6))
+
+# First axis (left y-axis)
+color = 'tab:red'
+ax3.set_xlabel('Threads')
+ax3.set_ylabel('Execution Time (s)', color=color)
+ax3.plot(threads, exec_time, color=color, label='Execution Time (s)', marker='o')
+ax3.tick_params(axis='y', labelcolor=color)
+
+# Legend
+lines, labels = ax3.get_legend_handles_labels()
+ax3.legend(lines, labels, loc='upper left', bbox_to_anchor=(1.05, 1))
+
+# Show plot
+plt.subplots_adjust(right=0.75)
+plt.title('Execution Time by Number of Threads')
+plt.xticks(threads, labels=threads)
+plt.xscale('log', base=2) 
+plt.grid(True)
+plt.savefig('time.png')
+
+
 plt.show()
